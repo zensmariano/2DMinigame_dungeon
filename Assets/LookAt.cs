391b55d90f16afe70ddfa02f6a3802fb842de104
zensmariano <titi_mariano@hotmail.com>
2018-11-10 20:13:40 +0000

    2d

---
+++ Assets/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	public Transform camera;
+	public Transform board;
+
+	// Use this for initialization
+	void Start () {
+		
+		camera.LookAt(board);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
