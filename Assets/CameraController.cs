725e6e056bf06dbcbe7b22520fbb56418323c838
Pedro cAugusto <pn.alegrete@hotmail.com>
2018-11-11 03:18:41 +0000

    k bro

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+        transform.position += inputHorizontal * Vector3.right * speed * Time.deltaTime;
+        transform.position += inputVertical * Vector3.up * speed * Time.deltaTime;
+    }
+}
