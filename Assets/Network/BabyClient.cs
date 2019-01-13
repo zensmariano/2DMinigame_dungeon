90e678484aa960e41e8d296b64595123b77d43b7
Pedro cAugusto <pn.alegrete@hotmail.com>
2019-01-13 23:04:44 +0000

    net feita

--- Assets/Network/BabyClient.cs
+++ Assets/Network/BabyClient.cs
@@ -4,100 +4,111 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
-using BabyBindings;
 
 public class BabyClient : MonoBehaviour {
 
-	public GameObjects_Info player_info, player2_info;
+    public GameObjectProperties player_properties, player2_properties;
     public GameObject player, player2;
     private Socket clientSocket;
     private byte[] buffer;
 
-    private Dictionary<short, GameObject> activePlayers;
-
-
     private void Start () {
-        player_info = new GameObjects_Info ();
-        player2_info = new GameObjects_Info();
-        activePlayers = new Dictionary<short, GameObject> ();
-
+        player_properties = new GameObjectProperties ();
+        player2_properties = new GameObjectProperties ();
         clientSocket = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         clientSocket.Connect (IPAddress.Loopback, Constants.Port);
 
-        buffer = new byte[GameObjects_Info.MaxWireSize];
+        buffer = new byte[GameObjectProperties.MaxSize * 2];
     }
 
     private void Update () {
-        player_info.Update (player.transform.position.x, player.transform.position.y); 
-		player_info.ToBuffer (ref buffer);
+        player_properties.Update (player.transform.position.x, player.transform.position.y,
+            player.transform.GetComponent<Animator>().GetFloat("Horizontal"), 
+            player.transform.GetComponent<Animator>().GetFloat("Vertical"),
+            player.transform.GetComponent<Animator>().GetBool("isMoving"),
+            player.transform.GetComponent<Animator>().GetBool("isAttacking"));
+        player_properties.ToBuffer (ref buffer);
         clientSocket.Send (buffer);
-        ReceiveInfo ();
-        ControlInfo ();
+        ReceiveProperties ();
+        ControlProperties ();
     }
 
-    private void ReceiveInfo () {
+    private void ReceiveProperties () {
         int data = clientSocket.Receive (buffer);
 
         if (data == 2) {
             short myNewID = BitConverter.ToInt16 (buffer, 0);
+            short otherPlayerID = 0;
+
+            switch (myNewID) {
+                case 1:
+                    otherPlayerID = 2;
+                    break;
+                case 2:
+                    otherPlayerID = 1;
+                    break;
+                default:
+                    break;
+            }
+
             player.GetComponent<NetID> ().ID = myNewID;
-            player_info.UpdateID (myNewID);
+            player2.GetComponent<NetID> ().ID = otherPlayerID;
+            player_properties.UpdateID (myNewID);
+            player2_properties.UpdateID (otherPlayerID);
             return;
         }
 
-        if(data == 8){
-            player.transform.position = new Vector2(BitConverter.ToSingle(buffer, 2), BitConverter.ToSingle(buffer, 6));
+        if (data == 8) {
+            player.transform.position = new Vector2 (BitConverter.ToSingle (buffer, 2), BitConverter.ToSingle (buffer, 6));
             return;
         }
 
-        for (int i = 0; i < data; i += 10) {
-            GameObjects_Info temp = new GameObjects_Info ();
+        if(data == 20){
+            player.transform.GetComponent<Animator>().SetFloat("Horizontal", BitConverter.ToSingle (buffer, 10));
+            player.transform.GetComponent<Animator>().SetFloat("Vertical", BitConverter.ToSingle (buffer, 14));
+            player.transform.GetComponent<Animator>().SetBool("isMoving", (bool)BitConverter.ToBoolean (buffer, 18));
+            player.transform.GetComponent<Animator>().SetBool("isAttacking", (bool)BitConverter.ToBoolean (buffer, 19));
+        }
+
+        for (int i = 0; i < data; i += 20) {
+            GameObjectProperties temp = new GameObjectProperties ();
             temp.FromBuffer (buffer, i);
             short tempID = BitConverter.ToInt16 (temp.PlayerID, 0);
 
-            if(tempID == player.GetComponent<NetID> ().ID)
-                continue; 
+            if (tempID == player.GetComponent<NetID> ().ID)
+                continue;
 
-            if (activePlayers.ContainsKey (tempID) == false) {
-                player2_info = temp;
-                CreateNewPlayer (tempID);
-            }
-
-            else {
-                Debug.Log(BitConverter.ToInt16 (player2_info.PlayerID, 0));
-                UpdateInfo(temp);
-            }
+            Debug.Log (BitConverter.ToInt16 (player2_properties.PlayerID, 0));
+            UpdateProperties (temp);
         }
     }
 
-    private void UpdateInfo (GameObjects_Info pInfo) {
-        
-        if((BitConverter.ToInt16 (player2_info.PlayerID, 0) == BitConverter.ToInt16 (pInfo.PlayerID, 0))){
-            player2_info.PositionX = pInfo.PositionX;
-            player2_info.PositionY = pInfo.PositionY;
-        }
-        
-    }
+    private void UpdateProperties (GameObjectProperties pInfo) {
 
-    private void CreateNewPlayer (short id) {
-        GameObject newInstance = Instantiate (player, Vector3.right, Quaternion.identity);
-        newInstance.AddComponent (typeof (NetID));
-        newInstance.GetComponent<NetID> ().ID = id;
-        activePlayers.Add (id, player);
-    }
-
-
-    private void ControlInfo () {
-        
-        foreach (var p in activePlayers) {
-             Vector2 newPosition = Vector2.zero;
-                
-                if (p.Key == BitConverter.ToInt16 (player2_info.PlayerID, 0)) {
-                 
-                    player2_info.Convert(ref newPosition.x, ref newPosition.y);
-                    p.Value.transform.position = new Vector3 (newPosition.x, newPosition.y);
-            }
+        if ((BitConverter.ToInt16 (player2_properties.PlayerID, 0) == BitConverter.ToInt16 (pInfo.PlayerID, 0))) {
+            player2_properties.PositionX = pInfo.PositionX;
+            player2_properties.PositionY = pInfo.PositionY;
+            player2_properties.Horizontal = pInfo.Horizontal;
+            player2_properties.Vertical = pInfo.Vertical;
+            player2_properties.IsMoving = pInfo.IsMoving;
+            player2_properties.IsAttacking = pInfo.IsAttacking;
         }
 
+    }
+
+    private void ControlProperties () {
+
+        Vector2 newPosition = Vector2.zero;
+        float horizontal = 0;
+        float vertical = 0;
+        bool is_moving = false;
+        bool is_attacking = false;
+
+        player2_properties.Convert (ref newPosition.x, ref newPosition.y, ref horizontal, ref vertical, ref is_moving, ref is_attacking);
+        player2.transform.position = new Vector3 (newPosition.x, newPosition.y);
+        player2.transform.GetComponent<Animator>().SetFloat("Horizontal", horizontal);
+        player2.transform.GetComponent<Animator>().SetFloat("Vertical", vertical);
+        player2.transform.GetComponent<Animator>().SetBool("isMoving", is_moving);
+        player2.transform.GetComponent<Animator>().SetBool("isAttacking", is_attacking);
     }
 }
---
+++ Assets/Network/GameObjectProperties.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+public class GameObjectProperties{
+
+	public static int MaxSize = 20;
+	public byte[] PlayerID, PositionX, PositionY, Horizontal, Vertical, IsMoving, IsAttacking;
+
+
+	public GameObjectProperties()
+    {
+		this.PlayerID = new byte[sizeof(short)];
+        this.PositionX = new byte[sizeof(float)];
+        this.PositionY = new byte[sizeof(float)];
+        this.Horizontal = new byte[sizeof(float)];
+        this.Vertical = new byte[sizeof(float)];
+        this.IsMoving = new byte[sizeof(bool)];
+        this.IsAttacking = new byte[sizeof(bool)];
+	}
+
+    public GameObjectProperties(short id)
+    {
+       	this.PlayerID = BitConverter.GetBytes(id);
+        this.PositionX = new byte[sizeof(float)];
+        this.PositionY = new byte[sizeof(float)];
+        this.Horizontal = new byte[sizeof(float)];
+        this.Vertical = new byte[sizeof(float)];
+        this.IsMoving = new byte[sizeof(bool)];
+        this.IsAttacking = new byte[sizeof(bool)];
+	}
+
+    public void Update(float posX, float posY, float horizontal, float vertical, bool isMoving, bool isAttacking)
+    {
+      	this.PositionX = BitConverter.GetBytes(posX);
+        this.PositionY = BitConverter.GetBytes(posY);
+        this.Horizontal = BitConverter.GetBytes(horizontal);
+        this.Vertical = BitConverter.GetBytes(vertical);
+        this.IsMoving = BitConverter.GetBytes(isMoving);
+        this.IsAttacking = BitConverter.GetBytes(isAttacking);
+ 	}
+
+    public void UpdateID(short id)
+    {
+    	this.PlayerID = BitConverter.GetBytes(id);
+    }
+
+   	public void ToBuffer(ref byte[] buffer)
+  	{
+    	System.Buffer.BlockCopy(PlayerID, 0, buffer, 0, PlayerID.Length);
+        System.Buffer.BlockCopy(PositionX, 0, buffer, PlayerID.Length, PositionX.Length);
+        System.Buffer.BlockCopy(PositionY, 0, buffer, PlayerID.Length + PositionX.Length, PositionY.Length);
+        System.Buffer.BlockCopy(Horizontal, 0, buffer, PlayerID.Length + PositionX.Length + PositionY.Length, Horizontal.Length);
+        System.Buffer.BlockCopy(Vertical, 0, buffer, PlayerID.Length + PositionX.Length + PositionY.Length + Horizontal.Length, Vertical.Length);
+        System.Buffer.BlockCopy(IsMoving, 0, buffer, PlayerID.Length + PositionX.Length + PositionY.Length + Horizontal.Length + Vertical.Length, IsMoving.Length);
+        System.Buffer.BlockCopy(IsAttacking, 0, buffer, PlayerID.Length + PositionX.Length + PositionY.Length + Horizontal.Length + Vertical.Length + IsMoving.Length, IsAttacking.Length);
+  	}
+
+    public void FromBuffer(byte[] buffer)
+    {
+     	System.Buffer.BlockCopy(buffer, 0, PlayerID, 0, sizeof(short));
+        System.Buffer.BlockCopy(buffer, sizeof(short), PositionX, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float), PositionY, 0, sizeof(float));
+       	System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float), Horizontal, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float), Vertical, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(float), IsMoving, 0, sizeof(bool));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(bool), IsAttacking, 0, sizeof(bool));
+  	}
+
+    public void FromBuffer(byte[] buffer, int offSet)
+	{
+    	System.Buffer.BlockCopy(buffer, offSet, PlayerID, 0, sizeof(short));
+        System.Buffer.BlockCopy(buffer, offSet + sizeof(short), PositionX, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, offSet + sizeof(short) + sizeof(float), PositionY, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float), Horizontal, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float), Vertical, 0, sizeof(float));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(float), IsMoving, 0, sizeof(bool));
+        System.Buffer.BlockCopy(buffer, sizeof(short) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(float) + sizeof(bool), IsAttacking, 0, sizeof(bool));
+   	}
+
+    public void Convert(ref float posX, ref float posY, ref float horizontal, ref float vertical, ref bool isMoving, ref bool isAttacking)
+	{
+		posX = BitConverter.ToSingle (this.PositionX, 0);
+		posY = BitConverter.ToSingle (this.PositionY, 0);
+		horizontal = BitConverter.ToSingle (this.Horizontal, 0);
+		vertical = BitConverter.ToSingle (this.Vertical, 0);
+		isMoving = BitConverter.ToBoolean (this.IsMoving, 0);
+		isAttacking = BitConverter.ToBoolean (this.IsAttacking, 0);
+	}
+}
+
+
